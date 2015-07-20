723b2482ed2d680fab57a9607834e44b23f90fdd
Anselme <nkanselme@gmail.com>
2015-07-20 14:18:25 +0200

    Initial Commit

---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Anselme Kotchap on 2015/07/20.
+//  Copyright (c) 2015 MIND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitUseExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitUseExample
+//
+//  Created by Anselme Kotchap on 2015/07/20.
+//  Copyright (c) 2015 MIND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
